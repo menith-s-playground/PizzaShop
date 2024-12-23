af573fc094b9db045ac58ba0b8a79bd683ae7bf4
Menith <menith.somarathne@gmail.com>
2024-12-23 06:24:51 +0530

    add pizza shop menu

---
+++ src/main/java/lk/kingston/cs/pizzaShopApp/view_controller/MenuViewController.java
@@ -0,0 +1,91 @@
+package lk.kingston.cs.pizzaShopApp.view_controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class MenuViewController {
+
+    @FXML
+    private ImageView bbqChickenImageView;
+
+    @FXML
+    private Button btnAddBBQChickenButton;
+
+    @FXML
+    private Button btnAddMargheritaButton;
+
+    @FXML
+    private Button btnAddPepperoniButton;
+
+    @FXML
+    private Button btnBBQFav;
+
+    @FXML
+    private Button btnMargheritFav;
+
+    @FXML
+    private Button btnMoreCheese;
+
+    @FXML
+    private Button btnPepperoniFav;
+
+    @FXML
+    private Button btnmoreChili;
+
+    @FXML
+    private ImageView margheritaImageView;
+
+    @FXML
+    private ImageView pepperoniImageView;
+
+    @FXML
+    private Button viewCartButton;
+
+    @FXML
+    void btnAddBBQChickenButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddMargheritaButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddPepperoniButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBBQFavOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMargheritFavOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMoreCheeseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPepperoniFavOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnmoreChiliOnACtion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCartButtonOnAction(ActionEvent event) {
+
+    }
+
+}
